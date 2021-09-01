432df4186eb886a92e1a621d88a363bc26d9aa05
platov2pac <ilya.platow.15@gmail.com>
2021-09-01 17:32:37 +0300

    login in process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login/login.component';
 import {RippleModule} from "primeng/ripple";
+import {HeaderComponent} from './components/header/header.component';
+import {WelcomeComponent} from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    WelcomeComponent
+
   ],
   imports: [
     BrowserModule,
     ButtonModule,
-    RippleModule
+    RippleModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/auth', pathMatch: 'full'},
+      {path: 'auth', component: LoginComponent},
+      {path:'welcome', component:WelcomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
