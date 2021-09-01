209499777f7cc7977e33976119f47a22d2a49d4c
platov2pac <ilya.platow.15@gmail.com>
2021-09-01 11:17:11 +0300

    primeNg added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {ButtonModule} from 'primeng/button';
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import {RippleModule} from "primeng/ripple";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
